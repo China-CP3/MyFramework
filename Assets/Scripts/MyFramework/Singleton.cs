96754c855f47c1667b1af1723fd212c320f63a88
China-CP3 <944122095@qq.com>
2023-03-27 14:16:29 +0800

    1

---
+++ Assets/Scripts/MyFramework/Singleton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> where T:new() 
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if(instance==null)
+            {
+                instance = new T();
+            }
+            return instance;
+        }      
+    }
+}
